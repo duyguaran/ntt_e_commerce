58cd063bb926d5f967ffb85927473f87f0d21050
duyguaran <duygu.aran15@gmail.com>
2023-03-21 01:51:30 +0300

    Update productCard.js

--- src/component/productCard.js
+++ src/component/productCard.js
@@ -42,35 +42,36 @@ const ProductCard = () => {
 
   return (
     <div className="row justify-content-center">
-      <div className="col-12 col-lg-10">
-        <div className="row">
-          {products.map((product) => (
-            <div className="col-md-3 mb-5" key={product.id}>
-              <div className="card h-100" style={{ width: "18rem", height: "20rem" }}>
-                <div
-                  style={{
-                    position: "absolute",
-                    top: 10,
-                    right: 10,
-                    color: favoriteItems.includes(product.id) ? "red" : "white",
-                  }}
-                  onClick={() => handleLike(product)}
-                >
-                  <FaHeart size={20} />
-                </div>
-                <img src={product.imageUrl} className="card-img-top" alt="..."  onClick={() => directUrl(product)}/>
-                <div className="card-body">
-                  <h5 className="card-title">{product.name}</h5>
-                  <p className="card-text">{product.description}</p>
-                  <p className="card-text">{product.price}</p>
-                  <p className="card-text">{product.shippingMethod}</p>
-                </div>
+    <div className="col-12 col-lg-10">
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+        {products.map((product) => (
+          <div className="col" key={product.id}>
+            <div className="card h-100" style={{ height: "20rem" }}>
+              <div
+                style={{
+                  position: "absolute",
+                  top: 10,
+                  right: 10,
+                  color: favoriteItems.includes(product.id) ? "red" : "white",
+                }}
+                onClick={() => handleLike(product)}
+              >
+                <FaHeart size={20} />
+              </div>
+              <img src={product.imageUrl} className="card-img-top" alt="..." onClick={() => directUrl(product)}/>
+              <div className="card-body">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">{product.description}</p>
+                <p className="card-text">{product.price}</p>
+                <p className="card-text">{product.shippingMethod}</p>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
+  </div>
+  
   );
 };
 
