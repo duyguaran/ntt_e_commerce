ac5df511dfbc8c9bc69f1e07470c2fcca7879329
duyguaran <duygu.aran15@gmail.com>
2023-03-21 01:44:24 +0300

    directUrl

--- src/component/productCard.js
+++ src/component/productCard.js
@@ -36,7 +36,7 @@ const ProductCard = () => {
     }
   };
 
-  const directUrl = (product) => {
+  const directUrl = () => {
     window.location.href = "https://www.google.com";
   };
 
@@ -45,7 +45,7 @@ const ProductCard = () => {
       <div className="col-12 col-lg-10">
         <div className="row">
           {products.map((product) => (
-            <div className="col-md-3 mb-5" key={product.id}onClick={() => directUrl(product)}>
+            <div className="col-md-3 mb-5" key={product.id}>
               <div className="card h-100" style={{ width: "18rem", height: "20rem" }}>
                 <div
                   style={{
@@ -58,7 +58,7 @@ const ProductCard = () => {
                 >
                   <FaHeart size={20} />
                 </div>
-                <img src={product.imageUrl} className="card-img-top" alt="..." />
+                <img src={product.imageUrl} className="card-img-top" alt="..."  onClick={() => directUrl(product)}/>
                 <div className="card-body">
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text">{product.description}</p>
